3a5e4b7b7b00cad2d9784a17cef7aa5306f950f4
Adrian Nuyda <adamnuyda@yahoo.com>
2016-02-23 18:09:17 -0800

    adds more info on List Users section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.all.order('created_at DESC')
     @posts = Post.all
     @post = Post.new
     @recentUsers = User.order('created_at DESC').limit(10)
