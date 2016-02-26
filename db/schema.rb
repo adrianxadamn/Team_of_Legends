6178183e2be5f2ea362d51cb5f74e17af3b0fa29
Adrian Nuyda <adamnuyda@yahoo.com>
2016-02-25 17:34:42 -0800

    removes info section

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224233915) do
+ActiveRecord::Schema.define(version: 20160226004338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20160224233915) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["team_id"], name: "index_users_on_team_id", using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+  name: "Adrian",
+  email: "admin@teamoflegends.com",
+  password: "blahblahblah",
+  password_confirmation: "blahblahblah",
+  admin: true
+
+
+  )
